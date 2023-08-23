d2fdf6ebe9dbb8a0e29163efac8af7e4a0a789a9
MuratOfficial <toimetm@gmail.com>
2023-08-23 23:04:03 +0600

    auth bugs fixed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,12 @@
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import prismadb from '@/lib/prismadb';
-import {compare} from 'bcrypt';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth, { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
-
+import GoogleProvider from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { compare } from 'bcrypt';
+import prismadb from '@/lib/prismadb';
 
-
-export default NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
@@ -18,61 +16,52 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
-    CredentialsProvider({
-        id: 'credentials',
-        name: 'Credentials',
-        credentials: {
-            email: {
-                label: 'Email',
-                type: 'text',
-            },
-            password: { label: "Password", type: "password" }
-
+    Credentials({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
         },
-        async authorize(credentials) {
-            
-            if(!credentials?.email || !credentials?.password) {
-                throw new Error('Email or password missing')
-            }
+        password: {
+          label: 'Password',
+          type: 'passord'
+        }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password required');
+        }
 
-            const user = await prismadb.user.findUnique({
-                where: {
-                    email: credentials.email,
-                }
-            });
+        const user = await prismadb.user.findUnique({ where: {
+          email: credentials.email
+        }});
 
-            if(!user || !user.hashedPassword) {
-                throw new Error('Email does not exist.')
-            }
+        if (!user || !user.hashedPassword) {
+          throw new Error('Email does not exist');
+        }
 
-            const isCorrectPassword = await compare(
-                credentials.password,
-                user.hashedPassword
-            )
+        const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
 
-            if(!isCorrectPassword) {
-                throw new Error('Incorrect Password')
-            }
+        if (!isCorrectPassword) {
+          throw new Error('Incorrect password');
+        }
 
-            return user;
-
-          }
+        return user;
+      }
     })
-    
-    
-    
-    
   ],
   pages: {
-    signIn: '/auth',
+    signIn: '/auth'
   },
-  debug: process.env.NODE_ENV==='development',
+  debug: process.env.NODE_ENV === 'development',
   adapter: PrismaAdapter(prismadb),
-  session: {
-    strategy: 'jwt',
-  },
+  session: { strategy: 'jwt' },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+export default NextAuth(authOptions);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,40 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
+import { Inter } from 'next/font/google';
+
+
+export async function getServerSideProps(context: NextPageContext) {
+const session = await getSession(context);
+
+if(!session) {
+  return {
+    redirect: {
+destination: '/auth',
+permanent: false,
+    }
+  }
+}
+
+return {
+  props: {},
+}
+}
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
+  const {data:user} = useCurrentUser();
+
   return (
-    <h1 className='text-2xl text-green-500'>Netflix Clone</h1>
+    <div>
+<h1 className='text-2xl text-green-500'>Netflix Clone</h1>
+<p className=' text-white'>Logged in as {user?.email}</p>
+<button className='f-10 w-16 bg-slate-400 ' onClick={()=>signOut()}>Log out</button>
+    </div>
+    
   )
 }
